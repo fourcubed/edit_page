00d2a03c3c1baa94838f2a3f169733c81459782f
Dan Olson <dolson@fourcubed.com>
2012-01-30 12:27:34 -0600

    add a create action

--- app/controllers/edit_page/pages_controller.rb
+++ app/controllers/edit_page/pages_controller.rb
@@ -3,6 +3,15 @@ module EditPage
     def index
       @pages = Page.all
     end
+    
+    def create
+      @page = Page.new(params[:page])
+      if @page.save
+        redirect_to url_for(:controller => 'edit_page/pages', :action => 'show', :id => @page.id)
+      else
+        render :action => :edit
+      end
+    end
   
     def edit
       @page = Page.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 EditPage::Engine.routes.draw do
   root :to => "pages#index"
-
+  
   match '/pages/:id' => 'pages#show'
   match '/pages/:id/edit' => 'pages#edit'
+  match '/pages/' => 'pages#create'
 
 end
