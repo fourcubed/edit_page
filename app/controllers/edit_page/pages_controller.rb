1e43121ed79116b95e62d1e2375d87bd308bd400
Dan Olson <dolson@fourcubed.com>
2012-02-01 13:20:08 -0600

    validates uniqueness and other niceities

--- app/controllers/edit_page/pages_controller.rb
+++ app/controllers/edit_page/pages_controller.rb
@@ -1,12 +1,16 @@
 module EditPage
   class PagesController < ApplicationController
     def index
-      @pages = Page.all
+      @pages = Page.where(:active => true)
     end
     
     def create
       @page = Page.new(params[:page])
+      @old_page = Page.find(params[:page][:id])
       if @page.save
+        @old_page.active = false
+        @old_page.save!
+        @page.activate!
         redirect_to url_for(:controller => 'edit_page/pages', :action => 'show', :id => @page.id)
       else
         render :action => :edit
--- app/models/edit_page/page.rb
+++ app/models/edit_page/page.rb
@@ -1,4 +1,10 @@
 module EditPage
   class Page < ActiveRecord::Base
+    #TODO: Validates uniqueness of (title + (active = true))
+    validates :title, :uniqueness => true, :if => :active?
+    
+    def active?
+      self.active == true
+    end
   end
 end
