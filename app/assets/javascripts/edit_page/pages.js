f0eff10e1c88d9b2893022b27ad1fd3b0610aa2a
Dan Olson <dolson@fourcubed.com>
2012-02-02 13:21:52 -0600

    testing load on demand

--- app/assets/javascripts/edit_page/pages.js
+++ app/assets/javascripts/edit_page/pages.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function setup() {
+  $('textarea').tinymce({
+    theme: 'advanced'
+  });
+}
