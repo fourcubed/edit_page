494ce29ee03fc8ec298d6d5608ffa80ea04e6fca
Dan Olson <dolson@fourcubed.com>
2012-02-02 16:12:27 -0600

    remove multiple redirects from create action, and dry up the code a bit

--- app/controllers/edit_page/pages_controller.rb
+++ app/controllers/edit_page/pages_controller.rb
@@ -13,20 +13,16 @@ module EditPage
     def create
       @page = Page.new(params[:page])
       if @page.save
-        redirect_to @page
+        redirect_to pages_path
       else
         render :action => :edit
-      end
-      respond_to do |format|
-        format.html { render :layout => nil }
-        format.js
       end
     end
   
     def edit
       @page = Page.find(params[:id])
       respond_to do |format|
-        format.html { render :layout => nil }
+        format.html
         format.js
       end
     end
@@ -43,7 +39,7 @@ module EditPage
         render :action => :edit
       end
       respond_to do |format|
-        format.html { render :layout => nil }
+        format.html
         format.js
       end
     end
