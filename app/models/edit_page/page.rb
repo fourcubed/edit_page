f9e516d6c1bc05b9840f9b85ba188257dd5c39fa
Dan Olson <dolson@fourcubed.com>
2012-03-02 11:21:09 -0600

    update validation

--- app/models/edit_page/page.rb
+++ app/models/edit_page/page.rb
@@ -2,7 +2,7 @@ module EditPage
   class Page < ActiveRecord::Base
     has_many :file_uploads, :as => :file
     validates :title, :uniqueness => true, :presence => true
-    validates :slug, :uniqueness => true, :presence => true, :format => {:with => /\w|-/}
+    validates :slug, :uniqueness => true, :presence => true, :format => { :with => /\w|-/, :on => :create }
     
     def active?
       self.active == true
