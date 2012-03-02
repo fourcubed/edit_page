efdfc0f2ccca489c18ae83218776f2808673c467
Dan Olson <dolson@fourcubed.com>
2012-03-02 11:16:20 -0600

    add validation for slug

--- app/models/edit_page/page.rb
+++ app/models/edit_page/page.rb
@@ -1,8 +1,8 @@
 module EditPage
   class Page < ActiveRecord::Base
     has_many :file_uploads, :as => :file
-    #TODO: Validates uniqueness of (title + (active = true))
     validates :title, :uniqueness => true, :presence => true
+    validates :slug, :uniqueness => true, :presence => true, :format => {:with => /\w|-/}
     
     def active?
       self.active == true
