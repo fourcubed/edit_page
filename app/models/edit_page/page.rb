e839b98751bad237fa876f4dcc2327d939c6ad53
Dan Olson <dolson@fourcubed.com>
2012-03-19 12:54:23 -0500

    custom validation messages

--- app/models/edit_page/page.rb
+++ app/models/edit_page/page.rb
@@ -2,7 +2,7 @@ module EditPage
   class Page < ActiveRecord::Base
     has_many :file_uploads, :as => :file
     validates :title, :uniqueness => true, :presence => true
-    validates :slug, :uniqueness => true, :presence => true, :format => { :with => /\w|-/, :on => :create }
+    validates :slug, :uniqueness => true, :presence => { :message => "URL can't be blank" }, :format => { :with => /\w|-/, :on => :create, :message => "URL is invalid" }
     
     def active?
       self.active == true
