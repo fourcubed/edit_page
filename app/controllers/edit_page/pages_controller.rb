48ca87a502db3ad79ee1ad1be621e01b0f9469ef
Dan Olson <dolson@fourcubed.com>
2012-03-19 12:07:04 -0500

    don't render new action

--- app/controllers/edit_page/pages_controller.rb
+++ app/controllers/edit_page/pages_controller.rb
@@ -28,7 +28,7 @@ module EditPage
         Rails.logger.debug("\n\n\nerrors = #{@page.errors.full_messages}\n\n\n")
         respond_to do |format|
           format.html { render :action => :new }
-          format.js { render :action => :new }
+          format.js
         end
       end
     end
