40deeeba6ccf134cbb95eb07e34128060ce918a5
Dan Olson <dolson@fourcubed.com>
2012-03-20 08:07:49 -0500

    update_attributes

--- app/controllers/edit_page/pages_controller.rb
+++ app/controllers/edit_page/pages_controller.rb
@@ -51,7 +51,7 @@ module EditPage
         @page.file_uploads.build(params[:file])
         params[:page]['content'] = "<img src='#{@page.file_uploads.last.upload.url}' alt='#{@page.file_uploads.last.upload_file_name.split(".").first}'>" + params[:page]['content']
       end
-      if @page.update_attributes!(params[:page])
+      if @page.update_attributes(params[:page])
         flash.now[:notice] = "Successfully updated page!"
         respond_to do |format|
           format.html { redirect_to @page }
