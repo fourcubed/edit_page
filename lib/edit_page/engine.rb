b8f3a9e8be8446c9b09b2caf549754c5c55a401d
Dan Olson <dolson@fourcubed.com>
2012-03-28 17:24:09 -0500

    custom validator

--- lib/edit_page/engine.rb
+++ lib/edit_page/engine.rb
@@ -4,11 +4,4 @@ module EditPage
   class Engine < Rails::Engine
     isolate_namespace EditPage
   end
-  class UriValidator < ActiveModel::EachValidator
-    def validate_each(object, attribute, value)
-      unless value =~ /^\w*$/
-        object.errors[attribute] << (options[:message] || "is not formatted properly")
-      end
-    end
-  end  
 end
