43eb5bcaa9af869007d4585451faa5502e0428b0
Dan Olson <dolson@fourcubed.com>
2012-03-28 12:47:08 -0500

    regex

--- app/models/edit_page/page.rb
+++ app/models/edit_page/page.rb
@@ -2,7 +2,7 @@ module EditPage
   class Page < ActiveRecord::Base
     has_many :file_uploads, :as => :file, :dependent => :destroy
     validates :title, :uniqueness => true, :presence => true
-    validates :relative_url, :uniqueness => true, :presence => true, :format => { :with => URI::REGEXP::PATTERN.const_get(:REL_URI) }
+    validates :relative_url, :uniqueness => true, :presence => true, :format => { :with => /#{URI::REGEXP::PATTERN.const_get(:REL_URI)}/ }
     
     def active?
       self.active == true
