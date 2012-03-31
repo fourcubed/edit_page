8f4c876a48e523cd9dc767c132a67c29fa7db660
DanOlson <olson_dan@yahoo.com>
2012-03-31 15:10:19 -0500

    wires up tinyMCE

--- app/assets/javascripts/edit_page/pages.js
+++ app/assets/javascripts/edit_page/pages.js
@@ -1,7 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-function setup() {
-  $('textarea').tinymce({
-    theme: 'advanced'
+
+function cmsInit() {
+  jQuery(document).ready(function(){
+    tinyMCE.init({
+      mode: 'textareas',
+      theme: 'advanced',
+      theme_advanced_toolbar_location: 'top'
+    });
   });
 }
