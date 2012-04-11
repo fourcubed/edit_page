68d87d0ff618c6907e9a8d15f5d498ce7d01b78d
Dan Olson <dolson@fourcubed.com>
2012-04-10 19:20:32 -0500

    allow for pdf uploads

--- app/controllers/edit_page/pages_controller.rb
+++ app/controllers/edit_page/pages_controller.rb
@@ -14,7 +14,13 @@ module EditPage
       @page = Page.new(params[:page])
       if params[:file]
         @page.file_uploads.build(params[:file])
-        @page.content.insert(0, "<img src='#{@page.file_uploads.last.upload.url}' alt='#{@page.file_uploads.last.upload_file_name.split(".").first}'>")
+        upload = @page.file_uploads.last
+        file_type = upload.upload_content_type.split('/').first
+        tag = case file_type
+          when "image"; "<img src='#{upload.upload.url}' alt='#{upload.upload_file_name.split(".").first}'>"
+          else "<a href='#{upload.upload.url}'>#{upload.upload_file_name}</a>"
+        end
+        @page.content.insert(0, tag)
       end
       if @page.save
         flash.now[:notice] = "Successfully created page!"
@@ -47,7 +53,12 @@ module EditPage
       @page = Page.find(params[:id])
       if params[:file]
         @page.file_uploads.build(params[:file])
-        params[:page]['content'] = "<img src='#{@page.file_uploads.last.upload.url}' alt='#{@page.file_uploads.last.upload_file_name.split(".").first}'>" + params[:page]['content']
+        upload = @page.file_uploads.last
+        file_type = upload.upload_content_type.split('/').first
+        params[:news_post]['content'] = case file_type
+          when "image"; "<img src='#{upload.last.upload.url}' alt='#{upload.last.upload_file_name.split(".").first}'>" + params[:news_post]['content']
+          else "<a href='#{upload.upload.url}'>#{upload.upload_file_name}</a>" + params[:news_post]['content']
+        end['content']
       end
       if @page.update_attributes(params[:page])
         flash.now[:notice] = "Successfully updated page!"
